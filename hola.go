4f55bbb064f36717c68e9a1eba8773dd9ae482c3
Gonzaloarin12 <gonzaarin12@gmail.com>
2024-04-16 12:32:42 -0300

    Backup

--- hola.go
+++ hola.go
@@ -11,7 +11,7 @@ import (
 const pi float32 = 3.14
 
 const (
-	Domingo = iota + 1
+	Domingo = iota + 1 // Agrega +1 a cada const //
 	Lunes
 	Martes
 	Miercoles
@@ -30,9 +30,18 @@ func main() {
 	//var firstName, lastName string
 	//var age int
 
-	firstName, lastName := "Gonzalo", "Arin"
-	age := 24
+	//firstName, lastName := "Gonzalo", "Arin"
+	//age := 24
 
-	fmt.Println(firstName, lastName, age)
-	fmt.Println(Domingo, Jueves)
+	// fmt.Println(firstName, lastName, age)
+	//fmt.Println(Domingo, Jueves)
+	var (
+		defaulint    int
+		defauluint   uint
+		defaulfloat  float32
+		defaulbool   bool
+		defaulstring string
+	)
+
+	fmt.Println(defaulint, defauluint, defaulfloat, defaulbool, defaulstring)
 }
