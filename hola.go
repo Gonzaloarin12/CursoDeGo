5277b68a4bd4392664f945c7f121af0c7546e8a4
Gonzalo Arin <gonzaloarin@MacBook-Pro-de-Gonzalo.local>
2024-04-15 23:09:07 -0300

    Primer commit

---
+++ hola.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+// Declaracion de constantes
+
+const pi float32 = 3.14
+
+func main() {
+
+	fmt.Println("Hola mundo")
+	fmt.Println(quote.Go())
+
+	//  Declaracion de variables
+
+	//var firstName, lastName string
+	//var age int
+
+	firstName, lastName := "Gonzalo", "Arin"
+	age := 24
+
+	fmt.Println(firstName, lastName, age)
+}
